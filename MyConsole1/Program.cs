639b6a70d6cfe693da6f6af103788fceb964f8eb
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2021-03-25 12:04:33 +0100

    Add files via upload
    add Myconsole1

---
+++ MyConsole1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
