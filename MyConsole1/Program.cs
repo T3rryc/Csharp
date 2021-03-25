3e312f9083d5bc5428ef1665eeaca51f5c77b351
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2021-03-25 12:36:55 +0100

    Update Program.cs
    Add Hello word by ALGOSUP

--- MyConsole1/Program.cs
+++ MyConsole1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace MyConsole1
 {
@@ -7,6 +7,7 @@ namespace MyConsole1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Word by ALGOSUP");
         }
     }
 }
